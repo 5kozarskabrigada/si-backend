797164cf60f26cbd4a9ab66b1f81995fa6f131fa
Tomris Mammadova <mammadova.tomris@gmail.com>
2026-01-20 23:00:45 +0400

    .

--- server.js
+++ server.js
@@ -15,8 +15,16 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Admin-ID', 'admin-id', 'X-Admin-ID'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Admin-ID',
+    'admin-id',
+    'X-Admin-ID',
+    'x-user-id',
+  ],
 }));
+
 app.use(express.json());
 
 
